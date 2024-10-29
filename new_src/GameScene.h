f4714b116815ca9076f8430613a59667b0d58e68
LvKNnT <khanglamtat@gmail.com>
2024-10-29 21:12:47 +0700

    small changes

--- new_src/GameScene.h
+++ new_src/GameScene.h
@@ -2,7 +2,6 @@
 #include "Properties.h"
 #include "Button.h"
 #include "RenderUtilities.h"
-#include <vector>
 #include "game.h"
 
 class GameScene {
@@ -11,11 +10,8 @@ public:
     ~GameScene() {}
 
     void Init() {
-        game.Init();
-        states.clear();
         state = MAIN;
     }
-
 
     // Is called per frame. Controls the flow of the game scene.
     void Run() {
@@ -50,10 +46,8 @@ private:
         ENDED       
     }; 
 
-    // Mode game and current scene
-    Game game;
+    // Current mode game and scene
     GAME_STATE state;
-    std::vector<GAME_STATE> states;
     bool is1PLayer;
 
     // Repeated buttons. Need to define first
@@ -63,22 +57,25 @@ private:
         // Side panel
         // Background
         DrawLine(GetScreenHeight(), 0, GetScreenHeight(), GetScreenHeight(), BLACK);
-        DrawTexturePro(Properties::elements["lmao"], (Rectangle) {0.0, 0.0, (float) Properties::elements["lmao"].width, (float) Properties::elements["lmao"].height}, (Rectangle) {0.0, 0.0, (float) Properties::screenWidth, (float) Properties::screenHeight}, (Vector2) {0.0, 0.0}, 0.0, WHITE);
+        DrawTexturePro(Properties::elements["gameBackGround"], (Rectangle) {0.0, 0.0, (float) Properties::elements["gameBackGround"].width, (float) Properties::elements["gameBackGround"].height}, (Rectangle) {0.0, 0.0, (float) Properties::screenWidth, (float) Properties::screenHeight}, (Vector2) {0.0, 0.0}, 0.0, WHITE);
 
         // Text box
         DrawText("This is the game, enjoy!", GetScreenHeight() + 30, 100, 25, RED);
         DrawText("Some information here for you nerds.", GetScreenHeight() + 30, 160, 15, LIGHTGRAY);
 
         game.Run();
+        if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+            state = PAUSE;
+        }
         if(backButton.Check()) {
             ChangeScene(TITLE_SCENE);
         }
     }
 
-    Button continueButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 3 / 8 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds[""], Properties::fonts["Rubik-Regular_45"], "Continue", 45, LIME);
-    Button saveButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() / 2 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds[""], Properties::fonts["Rubik-Regular_45"], "Save", 45, LIME);
-    Button loadButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 5 / 8- 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds[""], Properties::fonts["Rubik-Regular_45"], "Load", 45, LIME);
-    Button optionsButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 3 / 4 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds[""], Properties::fonts["Rubik-Regular_45"], "Options", 45, LIME);
+    Button continueButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 3 / 8 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds["buttonClick"], Properties::fonts["Rubik-Regular_45"], "Continue", 45, LIME);
+    Button saveButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() / 2 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds["buttonClick"], Properties::fonts["Rubik-Regular_45"], "Save", 45, LIME);
+    Button loadButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 5 / 8- 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds["buttonClick"], Properties::fonts["Rubik-Regular_45"], "Load", 45, LIME);
+    Button optionsButton = Button(Rectangle{(float)GetScreenWidth() / 2 - 130, (float)GetScreenHeight() * 3 / 4 - 30, 260, 70}, LIGHTGRAY, Properties::textures[""], Properties::sounds["buttonClick"], Properties::fonts["Rubik-Regular_45"], "Options", 45, LIME);
     void PauseGame() {
         // Render assets
         // Draw the tint over the background
@@ -87,56 +84,49 @@ private:
         
         // Move to other scenes
         if(continueButton.Check()) {
-            state = states.back();
-            states.pop_back();
+            state = MAIN;
         }
         if(saveButton.Check()) {
-            states.push_back(state);
             state = SAVE;
         }
         if(loadButton.Check()) {
-            states.push_back(state);
             state = LOAD;
         }
         if(optionsButton.Check()) {
             state = OPTIONS;
         }
-        if(backButton.Check()) {
-            state = states.back();
-            states.pop_back();
-        }
     }
 
     void SaveGame() {
         // Render assets
+        DrawTexturePro(Properties::elements["saveBackGround"], (Rectangle) {0.0, 0.0, (float) Properties::elements["gameBackGround"].width, (float) Properties::elements["gameBackGround"].height}, (Rectangle) {0.0, 0.0, (float) Properties::screenWidth, (float) Properties::screenHeight}, (Vector2) {0.0, 0.0}, 0.0, WHITE);
         DrawTextCenEx(Properties::fonts["Rubik-Regular_80"], "There is save title. Sorry", int(GetScreenWidth() / 2), int(GetScreenHeight() / 3), 80, 2, PINK);
 
         // Move to other scenes
         if(backButton.Check()) {
-            state = states.back();
-            states.pop_back();
+            state = PAUSE;
         }
     }
 
     void LoadGame() {
         // Render assets
+        DrawTexturePro(Properties::elements["loadBackGround"], (Rectangle) {0.0, 0.0, (float) Properties::elements["gameBackGround"].width, (float) Properties::elements["gameBackGround"].height}, (Rectangle) {0.0, 0.0, (float) Properties::screenWidth, (float) Properties::screenHeight}, (Vector2) {0.0, 0.0}, 0.0, WHITE);
+        DrawTextCenEx(Properties::fonts["Rubik-Regular_80"], "There is load title. Sorry", int(GetScreenWidth() / 2), int(GetScreenHeight() / 3), 80, 2, PINK);
 
         // Move to other scenes
-        DrawTextCenEx(Properties::fonts["Rubik-Regular_80"], "There is load title. Sorry", int(GetScreenWidth() / 2), int(GetScreenHeight() / 3), 80, 2, PINK);
         if(backButton.Check()) {
-            state = states.back();
-            states.pop_back();
+            state = PAUSE;
         }
     }
-
+    
     void OptionsGame() {
         // Render assets
+        DrawTexturePro(Properties::elements["optionsBackGround"], (Rectangle) {0.0, 0.0, (float) Properties::elements["gameBackGround"].width, (float) Properties::elements["gameBackGround"].height}, (Rectangle) {0.0, 0.0, (float) Properties::screenWidth, (float) Properties::screenHeight}, (Vector2) {0.0, 0.0}, 0.0, WHITE);
         
         // Move to other scenes
         DrawTextCenEx(Properties::fonts["Rubik-Regular_80"], "There is option title. Sorry", int(GetScreenWidth() / 2), int(GetScreenHeight() / 3), 80, 2, PINK);
         if(backButton.Check()) {
-            state = states.back();
-            states.pop_back();
+            state = PAUSE;
         }
     }
 };
--- new_src/Main.cpp
+++ new_src/Main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     // Init game window
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    // SetConfigFlags(FLAG_WINDOW_RESIZABLE); // temporary disable
     InitWindow(Properties::screenWidth, Properties::screenHeight, "CS160-chess-project");
     InitAudioDevice();
     SetTargetFPS(60);
@@ -26,6 +26,7 @@ int main() {
     while(!WindowShouldClose() && scene != EXIT) {
         Properties::screenHeight = GetScreenHeight();
         Properties::screenWidth = GetScreenWidth();
+        BeginDrawing(); {
             switch(scene) {
                 case TITLE_SCENE: {
                     if (isSceneChanged) titleScene.Init(), isSceneChanged = false; // init the scene if entering the scene
@@ -37,7 +38,6 @@ int main() {
                 }; break;
                 default: break;
             }
-        BeginDrawing(); {
         } EndDrawing();
     }
 
