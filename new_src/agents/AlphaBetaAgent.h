25edfc903fe9af21cd93479d186772479b99b978
LvKNnT <khanglamtat@gmail.com>
2024-11-25 12:59:47 +0700

    add lastest annChann commit

--- new_src/agents/AlphaBetaAgent.h
+++ new_src/agents/AlphaBetaAgent.h
@@ -1,12 +1,15 @@
+/*
+Alphabeta-pruning minimax search tree, with move ordering for first few depths.
+*/
+
 #ifndef ALPHA_BETA_AGENT_H
 #define ALPHA_BETA_AGENT_H
 
 #include "Agent.h"
-#include <map>
 
 class AlphaBetaAgent : public Agent {
 public:
-    AlphaBetaAgent(CHESS_COLOR agentColor, float moveDelay = 0.2);
+    AlphaBetaAgent(CHESS_COLOR agentColor, float moveDelay = 0.12);
     void Init() override;
     std::optional<Move> GetMove(const Board &board) override;
     
@@ -14,11 +17,9 @@ private:
     const float moveDelay; // how long should the bot wait to move (0 for instant play)
     float timeDelayed; // time passed since beginning of the move making
     const int depth = 4;
-    std::map<long long, float> cache;
+    const int sortDepth = 2;
 
     Move _GetMove(const Board &board);
-    float Evaluate(const Board &board) const;
-    long long Hash(const Board &board) const;
 
 };
 
